940c1c4a2d8f0038c192087ad456de13df8b60e1
Anshul Agrawal <51487360+kolas19@users.noreply.github.com>
2023-12-03 03:43:38 -0600

    Delete .local/bin directory

--- .local/bin/go2dir.sh
+++
@@ -1,5 +0,0 @@
-if [[ $( git root ) != *"not a git repo"* ]]; then
-    cd $( fd -t d . --hidden | fzf )
-else
-    cd $( fd -t d --full-path $( git root ) --hidden | fzf )
-fi
--- .local/bin/ls.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/env sh
-
-exa -la --icons --group-directories-first --color=always $@ | rg -v "\.DS_Store"
